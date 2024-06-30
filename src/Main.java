227b6b672a5520b8a204aa71d515dfb5107f8041
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-06-30 20:16:45 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean maturity = (age >= 18);
+        if (maturity) {
+            System.out.println("Если возраст человека равен " + age + "он совершеннолетний");
+        }
+
+
+
+    }
+
+}
